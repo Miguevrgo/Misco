abf8cb087959ff9b711349bae8abd87b7b28a109
Miguevrgo <miguevrgo@gmail.com>
2025-07-19 14:27:38 +0200

    Upload main info

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    println!("\x1b[1;33m╔══════════════════════════════════════════════╗\x1b[0m");
+    println!(
+        "\x1b[1;33m║\x1b[1;34m           Misco Stock Predictor V0           \x1b[1;33m║\x1b[0m"
+    );
+    println!(
+        "\x1b[1;33m║\x1b[1;34m    Miguel Angel De la Vega | Gonzalo Olmo    \x1b[1;33m║\x1b[0m"
+    );
+    println!("\x1b[1;33m╚══════════════════════════════════════════════╝\x1b[0m");
 }
