6eaaba9343cad958d87c1a133ffc387dfeb308af
Gonzalo <golmofernandez@gmail.com>
2025-08-02 17:53:17 +0200

    upload buy and sell stocks simple simulation

--- src/main.rs
+++ src/main.rs
@@ -149,6 +149,87 @@ fn test() {
     }
 }
 
+#[cfg(feature = "stonks")]
+fn stonks() {
+    let mut portfolio = Portfolio::new();
+    let mut funds = 1000.0;
+    let initial_funds = funds;
+    let mut shares = 0.0;
+    for (ticker, name) in LEARN_TICKER[0..1].iter().zip(LEARN_NAME[0..1].iter()) {
+        let filename = format!("data/{ticker}.csv");
+        let path = Path::new(&filename);
+        portfolio.load_stock(ticker, name, path);
+    }
+
+    let network = Network::load_from_file("./data/networks/network.bin").unwrap();
+    const CHUNK_SIZE: usize = 512;
+    let num_predictions = 300;
+
+    let stock = portfolio.stock(LEARN_TICKER[0]).expect("Stock not found");
+
+    let total_entries = stock.entries.len();
+    if total_entries < CHUNK_SIZE {
+        panic!("Not enough historical data for prediction");
+    }
+
+    let mut file = File::create("predictions.csv").unwrap();
+    writeln!(file, "Date,Predicted Value,Real Value,Error").unwrap();
+    let mut last_real: f32 = 0.0;
+    for i in (0..num_predictions).rev() {
+        let entries =
+            &stock.entries[(stock.entries.len() - CHUNK_SIZE - i - 1)..stock.entries.len() - i - 1];
+
+        let mut test_data =
+            portfolio.get_data(&LEARN_TICKER, entries[0].0, entries.last().unwrap().0);
+        test_data.normalize();
+
+        let test_entry = &test_data.data[0];
+        assert_eq!(test_entry.training_input.len(), 512);
+
+        let input: Array1<f32> = Array1::from(
+            test_entry
+                .training_input
+                .iter()
+                .map(|e| e.close)
+                .collect::<Vec<f32>>(),
+        );
+
+        let prediction = test_entry.denormalize(network.feed_forward(&input)[[0]]);
+        let real = test_entry.denormalize(test_entry.real_value);
+
+        if i == num_predictions - 1 {
+            last_real = real;
+            continue;
+        }
+
+        println!("Today prize: {last_real}");
+        println!("Tomorrow prediction: {prediction}");
+
+        if i == 0 {
+            funds += shares * last_real;
+            shares = 0.0;
+            println!("Simulation ended");
+            println!("Funds: ${funds}");
+            println!("Performance: {}%", ((funds/initial_funds)-1.0)*100.0);
+
+        } else if prediction < last_real && shares != 0.0 {
+            funds = last_real * shares;
+            shares = 0.0;
+            println!("Selling all shares");
+            println!("Funds: ${funds}");
+            println!("Shares: {shares}");
+        } else if prediction > last_real && funds > 0.0 {
+            shares = funds / last_real;
+            funds = 0.0;
+            println!("Buying shares");
+            println!("Funds: ${funds}");
+            println!("Shares: {shares}");
+        }
+        last_real = real;
+        println!("---")
+    }
+}
+
 fn main() {
     print_header();
     #[cfg(feature = "train")]
@@ -157,4 +238,6 @@ fn main() {
     predict();
     #[cfg(feature = "test")]
     test();
+    #[cfg(feature = "stonks")]
+    stonks();
 }
